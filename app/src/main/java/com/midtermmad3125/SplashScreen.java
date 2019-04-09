320a73c46ba2d766ccfc5333d735ff62727c7a43
sumitk68 <imsumitk1@gmail.com>
2019-04-08 20:53:26 -0400

    Splash Screen Delay

--- app/src/main/java/com/midtermmad3125/SplashScreen.java
+++ app/src/main/java/com/midtermmad3125/SplashScreen.java
@@ -1,13 +1,27 @@
 package com.midtermmad3125;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.midtermmad3125.ui.MainCityActivity;
+
 public class SplashScreen extends AppCompatActivity {
+    private final int SPLASH_DISPLAY_LENGTH = 1000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                Intent mainIntent = new Intent(SplashScreen.this,MainCityActivity.class);
+                startActivity(mainIntent);
+                finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
     }
 }
